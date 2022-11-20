fb2cb764e950bdcf3c6bf020d90851b27eee6061
wuttichai-beamcheckout <wuttichai@beamcheckout.com>
2022-11-20 19:55:25 +0700

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js stg!</a>
+          123 Welcome to <a href="https://nextjs.org">Next.js stg!</a>
         </h1>
 
         <p className={styles.description}>
